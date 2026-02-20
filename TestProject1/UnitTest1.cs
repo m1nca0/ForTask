214df478a5ce6e1c91fbd1afebda419e9b44d309
m1nca0 <noname890256@gmail.com>
2026-02-20 18:42:39 +0800

    add Tests

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using ForTask;
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FindBiggestTest()
+        {
+            float[] floatMassive = new float[] {1, 2, 1 , 3, 2};
+            int length = floatMassive.Length;
+            var countFloat = Logic.FindBiggest(length, floatMassive);
+            float[] compareMassive = new float[] {2,3};
+            Assert.That(compareMassive, Is.EqualTo(countFloat));
+        }
+    }
+}
