ca99821cec3cc177c0baa5b87e91b3a19d3c538b
Mihail VASILEV <noname890256@gmail.com>
2026-03-03 23:59:00 +0800

    rewrite tests

--- Tests/TestProject/UnitTest1.cs
+++ Tests/TestProject/UnitTest1.cs
@@ -1,3 +1,4 @@
+using ForTask;
 namespace TestProject;
 
 public class Tests
@@ -8,8 +9,57 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void FindBiggestTest()
     {
-        Assert.Pass();
+        double[] doubleMassive = new double[] { 1.3, 2.35, 1.83, 3.13, 2.2 };
+        int length = doubleMassive.Length;
+        var countdouble = Logic.FindBiggest(length, doubleMassive);
+        double[] compareMassive = new double[] { 2.35, 3.13 };
+        Assert.That(compareMassive, Is.EqualTo(countdouble));
+    }
+    [Test]
+    public void NullCompareTest()
+    {
+        double[] doubleMassive = new double[] { };
+        int length = doubleMassive.Length;
+        var countdouble = Logic.FindBiggest(length, doubleMassive);
+        double[] compareMassive = new double[] { };
+        Assert.That(compareMassive, Is.EqualTo(countdouble));
+    }
+    [Test]
+    public void AllSameTest()
+    {
+        double[] doubleMassive = new double[] { 1, 1, 1, 1, 1 };
+        int length = doubleMassive.Length;
+        var countdouble = Logic.FindBiggest(length, doubleMassive);
+        double[] compareMassive = new double[] { };
+        Assert.That(compareMassive, Is.EqualTo(countdouble));
+    }
+    [Test]
+    public void FirstLowestSecondSameTest()
+    {
+        double[] doubleMassive = new double[] { 1, 2, 2 };
+        int length = doubleMassive.Length;
+        var countdouble = Logic.FindBiggest(length, doubleMassive);
+        double[] compareMassive = new double[] { };
+        Assert.That(compareMassive, Is.EqualTo(countdouble));
+    }
+    [Test]
+    public void FirstSameSecondLowestTest()
+    {
+        double[] doubleMassive = new double[] { 2, 2, 1 };
+        int length = doubleMassive.Length;
+        var countdouble = Logic.FindBiggest(length, doubleMassive);
+        double[] compareMassive = new double[] { };
+        Assert.That(compareMassive, Is.EqualTo(countdouble));
+    }
+    [Test]
+    public void CompareDoubleTest()
+    {
+        double[] doubleMassive = new double[] { 2.2, 2.3, 2.1 };
+        int length = doubleMassive.Length;
+        var countdouble = Logic.FindBiggest(length, doubleMassive);
+        double[] compareMassive = new double[] { 2.3 };
+        Assert.That(compareMassive, Is.EqualTo(countdouble));
     }
 }
